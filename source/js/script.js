72d8e1b7f344b64add2d352202738d3eb02a81f3
Olia <good-friday@mail.ru>
2021-05-04 19:13:13 +0300

    Доработка свайпа

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,9 @@
 'use strict';
+
+// МЕНЮ:
 var mainNav = document.querySelector('.main-nav');
 var menuToggle = document.querySelector('.main-nav__toggle');
-
 mainNav.classList.remove('main-nav--nojs');
-
 menuToggle.addEventListener('click', function () {
   if (mainNav.classList.contains('main-nav--closed')) {
     mainNav.classList.remove('main-nav--closed');
@@ -14,9 +14,8 @@ menuToggle.addEventListener('click', function () {
   }
 });
 
+// ПЕРЕКЛЮЧЕНИЕ ТАБОВ:
 const tabs = document.querySelectorAll('.tabs__link');
-const placesLinks = document.querySelectorAll('.places__link');
-
 tabs.forEach((tab) => {
   tab.addEventListener('click', (evt) => {
     evt.preventDefault();
@@ -29,15 +28,15 @@ tabs.forEach((tab) => {
   });
 });
 
+// ПЕРЕКЛЮЧЕНИЕ ТАБОВ ПРИ КЛИКЕ СМОТРЕТЬ ПОДРОБНЕЕ:
+const placesLinks = document.querySelectorAll('.places__link');
 placesLinks.forEach((elem) => {
   elem.addEventListener('click', () => {
     const placeName = elem.dataset.tab;
     const placeItem = document.querySelector('.countries__item--' + placeName);
     document.querySelector('.countries__item--selected').classList.remove('countries__item--selected');
     placeItem.classList.add('countries__item--selected');
-
     document.querySelector('.tabs__link--selected').classList.remove('tabs__link--selected');
-
     tabs.forEach((tab) => {
       if (elem.dataset.tab === tab.dataset.tab) {
         tab.classList.add('tabs__link--selected');
@@ -46,8 +45,8 @@ placesLinks.forEach((elem) => {
   });
 });
 
+// ПЛАВНЫЙ СКРОЛЛ ССЫЛОК-ЯКОРЕЙ:
 const smoothLinks = document.querySelectorAll('a[href^="#"]');
-
 smoothLinks.forEach((link) => {
   link.addEventListener('click', (evt) => {
     evt.preventDefault();
@@ -59,45 +58,55 @@ smoothLinks.forEach((link) => {
   });
 });
 
+// СВАЙП:
 
-const swipeBlock = document.querySelector('.tabs__list');
-let initialPosition = null;
-let moving = false;
-let transform = 0;
-let transformValue = 0;
-let lastPageX = 0;
+const tabletWidth = 1023;
 
-const handleTouchStart = (evt) => {
-  initialPosition = evt.touches[0].pageX;
-  moving = true;
-  const transformMatrix = window.getComputedStyle(swipeBlock).getPropertyValue('transform');
-  if (transform !== 'none') {
-    transform = parseInt(transformMatrix.split(',')[4].trim());
-  }
-};
+if (document.body.clientWidth < tabletWidth) {
+  const swipeBlock = document.querySelector('.tabs__list');
+  const swipeContainer = document.querySelector('.tabs');
+  const swipeContainerLeftMargin = window.getComputedStyle(swipeBlock).getPropertyValue('margin').split(' ')[1].slice(0, -2);
+  const swipeContainerSideMargins = parseInt(swipeContainerLeftMargin) * 2;
 
-const handleTouchMove = (evt) => {
-  if (moving) {
-    const currentPosition = evt.touches[0].pageX;
-    const diff = currentPosition - initialPosition;
-    if (evt.touches[0].pageX - lastPageX > 0) {
-      if (transformValue > 0) {
-        return;
-      } else if (Math.abs(transformValue) > swipeBlock.offsetWidth) {
-        return;
+  let initialPosition = 0;
+  let moving = false;
+  let transform = 0;
+  let transformValue = 0;
+  let lastPageX = 0;
+
+  const handleTouchStart = (evt) => {
+    initialPosition = evt.pageX;
+    moving = true;
+    const transformMatrix = window.getComputedStyle(swipeBlock).getPropertyValue('transform');
+    if (transformMatrix !== 'none') {
+      transform = parseInt(transformMatrix.split(',')[4].trim());
+    };
+  };
+
+  const handleTouchMove = (evt) => {
+    if (moving) {
+      const currentPosition = evt.pageX;
+      const diffX = currentPosition - initialPosition;
+      transformValue = parseInt(transform) + diffX;
+      if (evt.pageX - lastPageX > 0) {
+        if (transformValue > 0) {
+          return;
+        }
+      } else {
+        if (Math.abs(transformValue) > swipeBlock.offsetWidth - swipeContainer.offsetWidth + swipeContainerSideMargins) {
+          return;
+        }
       }
+      swipeBlock.style.transform = `translateX(${transformValue}px)`;
     }
-    transformValue = transform + diff;
-    console.log(transformValue);
-    swipeBlock.style.transform = `translateX(${transformValue}px)`;
+    lastPageX = evt.pageX;
+  };
+
+  const handleTouchEnd = () => {
+    moving = false;
   }
-  lastPageX = evt.touches[0].pageX;
-};
 
-const handleTouchEnd = (evt) => {
-  moving = false;
+  swipeBlock.addEventListener('pointerdown', handleTouchStart);
+  swipeBlock.addEventListener('pointermove', handleTouchMove);
+  swipeBlock.addEventListener('pointerup', handleTouchEnd);
 }
-
-swipeBlock.addEventListener('touchstart', handleTouchStart);
-swipeBlock.addEventListener('touchmove', handleTouchMove);
-swipeBlock.addEventListener('touchend', handleTouchEnd);
